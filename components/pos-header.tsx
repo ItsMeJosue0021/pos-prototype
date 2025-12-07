69bbbfc8c0762e69584bb2249439b8856f5aa856
Joshua Salceda <100130292+ItsMeJosue0021@users.noreply.github.com>
2025-12-07 23:23:12 +0800

    updates

--- components/pos-header.tsx
+++ components/pos-header.tsx
@@ -1,19 +1,37 @@
-import { Label } from "@radix-ui/react-label";
+"use client"
+
+import { IoMdNotificationsOutline } from "react-icons/io";
 import { 
     IoMenuOutline, 
     IoSettingsOutline 
 } from "react-icons/io5";
+import IconButton from "@mui/material/IconButton";
 
 const PointOfSaleHeader = () => {
     return (
         <div className="fixed top-0 left-0 w-full p-4 flex items-center justify-between bg-white border-b border-gray-200 z-40">
-            <div className="flex items-center gap-3">
-                <IoMenuOutline size={32} className="text-gray-600"/>
-                {/* <p className="font-semibold text-gray-800">Joshua Salceda</p> */}
+            <div className="flex items-center bg-blue-700 rounded-tl-md rounded-br-md">
+                <div className="scale-75 flex items-center gap-2 ">
+                    <p className=" font-bold text-white text-xl py-1 px-2  border-blue-600">SOXIO</p>
+                </div>
             </div>
-            <IoSettingsOutline size={25} className="text-gray-600"/>
+
+            <div className="flex items-center gap-2">
+                <IconButton className="w-9 h-9 rounded hover:bg-blue-50 flex items-center justify-center group cursor-pointer">
+                    <IoMdNotificationsOutline 
+                        size={32} 
+                        className="text-gray-600 group-hover:text-blue-700" />
+                </IconButton>
+                <IconButton className="w-9 h-9 rounded hover:bg-blue-50 flex items-center justify-center group cursor-pointer">
+                    <IoMenuOutline 
+                        size={32} 
+                        className="text-gray-600 group-hover:text-blue-700" />
+                </IconButton>
+            </div>
         </div>
     )
 }
 
 export default PointOfSaleHeader;
+
+ {/* <IoMenuOutline size={32} className="text-gray-600"/> */}
